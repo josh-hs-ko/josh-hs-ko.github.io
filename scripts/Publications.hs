ba3358a6800b61bcb214dab77f9564a182898b95
Josh Ko <hsiang-shang@nii.ac.jp>
2017-10-25 11:16:26 +0900

    Update links to SSBX’16 material.

--- scripts/Publications.hs
+++ scripts/Publications.hs
@@ -43,8 +43,8 @@ publicationList =
       , year    = 2017
       , types   = [ (Published, "Publication planned")
                   ]
-      , links   = [ ("PDF", Just "preprint", "https://bitbucket.org/prl_tokyo/bigul/raw/master/SSBX16/tutorial.pdf")
-                  , ("Haskell code", Nothing, "https://bitbucket.org/prl_tokyo/bigul/src/master/SSBX16")
+      , links   = [ ("PDF", Just "preprint", "manuscripts/SSBX16-BiGUL.pdf")
+                  , ("Haskell code", Nothing, "https://bitbucket.org/prl_tokyo/bigul/src/master/SSBX16/SSBX16-BiGUL-code.zip")
                   ]
       , info    = [ ("Abstract", "Putback-based bidirectional programming allows the programmer to write only one putback transformation, from which the unique corresponding forward transformation is derived for free. A key distinguishing feature of putback-based bidirectional programming is full control over the bidirectional behavior, which is important for specifying intended bidirectional transformations without any ambiguity. In this chapter, we will introduce BiGUL, a simple yet powerful putback-based bidirectional programming language, explaining the underlying principles and showing how various kinds of bidirectional application can be developed in BiGUL.", Nothing)
                   ]
