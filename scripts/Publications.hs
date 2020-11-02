a7a8f627d5be79ccf16a79746c219bf88d711cb8
Josh Ko <>
2020-11-02 10:37:27 +0800

    Metamorphisms in Agda paper published

--- scripts/Publications.hs
+++ scripts/Publications.hs
@@ -29,13 +29,14 @@ publicationList =
                   ]
       , venue   = Just ("The Art, Science, and Engineering of Programming", Nothing)
       , year    = 2020
-      , types   = [ (Published, "Accepted")
+      , types   = []
+      , links   = [ ("PDF", Nothing, "https://arxiv.org/pdf/2010.16302")
+                  , ("Agda code", Just "Zenodo", "https://doi.org/10.5281/zenodo.4030101")
                   ]
-      , links   = [ ("PDF", Just "draft", "https://bitbucket.org/josh-hs-ko/MetamorphismsInAgda/raw/master/MetamorphismsInAgda.pdf")
-                  , ("Agda code", Just "Bitbucket", "https://bitbucket.org/josh-hs-ko/MetamorphismsInAgda/src/master/MetamorphismsInAgda.agda")
-                  ]
-      , info    = [ ("Volume", "5", Nothing)
-                  , ("Issue" , "2", Nothing)
+      , info    = [ ("Volume",  "5", Nothing)
+                  , ("Issue" ,  "2", Nothing)
+                  , ("Article", "7", Nothing)
+                  , ("DOI", "10.22152/programming-journal.org/2021/5/7", Nothing)
                   , ("Related blog posts", "<a href=\"/blog/0003/\">0003</a>", Nothing)
                   , ("Abstract", "In <em>dependently typed programming</em>, proofs of basic, structural properties can be embedded implicitly into programs and do not need to be written explicitly. Besides saving the effort of writing separate proofs, a most distinguishing and fascinating aspect of dependently typed programming is that it makes the idea of <em>interactive type-driven development</em> much more powerful, where expressive type information becomes useful hints that help the programmer to complete a program. There have not been many attempts at exploiting the full potential of the idea, though. As a departure from the usual properties dealt with in dependently typed programming, and as a demonstration that the idea of interactive type-driven development has more potential to be discovered, we conduct an experiment in ‘type-driven algorithm design’: we develop algorithms from their specifications encoded in sophisticated types, to see how useful the hints provided by a type-aware interactive development environment can be. The algorithmic problem we choose is <em>metamorphisms</em>, whose definitional behaviour is consuming a data structure to compute an intermediate value and then producing a codata structure from that value, but there are other ways to compute metamorphisms. We develop Gibbons’s streaming algorithm and Nakano’s jigsaw model in the interactive development environment provided by the dependently typed language Agda, turning intuitive ideas about these algorithms into formal conditions and programs that are correct by construction.", Nothing)
                   ]
@@ -56,7 +57,8 @@ publicationList =
       , types   = [ (Published, "Accepted")
                   ]
       , links   = []
-      , info    = [ ("URL", "https://www.usenix.org/conference/osdi20/presentation/chang", Nothing)
+      , info    = [ ("Pages", "81–97", Nothing)
+                  , ("URL", "https://www.usenix.org/conference/osdi20/presentation/chang", Nothing)
                   , ("Related blog posts", "<a href=\"/blog/0004/\">0004</a>, <a href=\"/blog/0007/\">0007</a>", Nothing)
                   , ("Abstract", "We introduce the design of a snapshot-consistent flash translation layer (SCFTL) for flash disks, which has a stronger guarantee about the possible behaviors after a crash than conventional designs. More specifically, the flush operation of SCFTL also has the functionality of making a “disk snapshot.” When a crash occurs, the flash disk is guaranteed to recover to the state right before the last flush. The major benefit of SCFTL is that it allows a more efficient design of upper layers in the storage stack. For example, the file system hosted by SCFTL does not require the use of a journal for crash recovery. Instead, it only needs to perform a flush operation of SCFTL at the end of each atomic transaction. We use a two-layer approach, combining a proof assistant, a symbolic executor, and an SMT solver, to formally verify the correctness of our prototype SCFTL implementation. We optimize the xv6 file system by utilizing SCFTL’s stronger crash guarantee. Evaluation results show that the optimized xv6 is 3 to 30 times faster than the original version.", Nothing)
                   ]
